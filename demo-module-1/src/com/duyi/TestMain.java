f35fefa7d7036493033e43781771850812228bab
OPHawk4 <517002938@qq.com>
2022-01-24 23:50:06 +0800

    终于弄明白如何提交项目到GitHub上了

--- demo-module-1/src/com/duyi/TestMain.java
+++ demo-module-1/src/com/duyi/TestMain.java
@@ -2,7 +2,7 @@ package com.duyi;
 
 public class TestMain {
     public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("Git");
+        System.out.println("Hello GitHub");
+
     }
 }
