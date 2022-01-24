47487ea33159fc44aa0f4800b334b16edf84ea50
OPHawk4 <517002938@qq.com>
2022-01-24 23:15:28 +0800

    2315

--- demo-module-1/src/com/duyi/TestMain.java
+++ demo-module-1/src/com/duyi/TestMain.java
@@ -3,6 +3,6 @@ package com.duyi;
 public class TestMain {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("GitXs");
+        System.out.println("Git");
     }
 }
