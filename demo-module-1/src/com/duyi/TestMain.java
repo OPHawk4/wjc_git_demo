4d40da7d2bb6b025b5bc385e19d9c4517a493ef7
OPHawk4 <517002938@qq.com>
2022-01-24 20:27:57 +0800

    我第一次使用Git提交代码到GitHub

--- demo-module-1/src/com/duyi/TestMain.java
+++ demo-module-1/src/com/duyi/TestMain.java
@@ -3,6 +3,6 @@ package com.duyi;
 public class TestMain {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Git上没显示？");
+        System.out.println("Git上没显示");
     }
 }
