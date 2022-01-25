550510539944217bcd3885531a898063cca31ce4
OPHawk4 <517002938@qq.com>
2022-01-25 12:17:34 +0800

    test修改 增加 hello test

--- demo-module-1/src/com/duyi/TestMain.java
+++ demo-module-1/src/com/duyi/TestMain.java
@@ -3,6 +3,6 @@ package com.duyi;
 public class TestMain {
     public static void main(String[] args){
         System.out.println("Hello GitHub");
-
+        System.out.println("Hello Test");
     }
 }
